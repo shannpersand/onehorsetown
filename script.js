6dfff47bb7558f99ecc62c9759d80714430a3c55
shannpersand <shannpersand@gmail.com>
2026-01-16 13:29:20 -0800

    Update case study, bpo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var d = new Date();
 var month = d.getMonth()+1;
 var day = d.getDate();
 
-var output = 
+var output =
     ((''+month).length<2 ? '0' : '') + month + '/' +
     ((''+day).length<2 ? '0' : '') + day
     // + '/' + d.getFullYear();
